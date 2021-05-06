ac86780e2a0823a5b5dc9b9822425b0b1f16eadf
Jasdhir <jasdhir.training@gmail.com>
2021-05-06 21:27:54 +0530

    Get mapping

--- src/main/java/com/revature/sonar/controller/HelloController.java
+++ src/main/java/com/revature/sonar/controller/HelloController.java
@@ -1,12 +1,13 @@
 package com.revature.sonar.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	public String sayHello() {
 		return "hello";
 	}
